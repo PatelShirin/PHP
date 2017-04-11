6459f757285af8f04f063c24526b0de9c2155472
PatelShirin <patel.shirin@outlook.com>
2017-04-10 22:37:48 -0400

    minor changes to quiz.php

--- CH3/Views/quiz.php
+++ CH3/Views/quiz.php
@@ -1,3 +1,17 @@
 <?php
-$output = include_once "views/quiz-form.php";
+$quizISSubmitted = isset( $_POST['quiz-submitted']);
+if( $quizISSubmitted ){
+    $answer = $_POST['answer'];
+    $output = showQuizResponse( $answer );
+} else {
+    $output = include_once "views/quiz-form.php";
+}
 return $output;
+
+function showQuizResponse ( $answer ) {
+    $response = "<p>You clicked $answer</p>";
+    $response .= "<p>
+                    <a href = 'index.php?page=quiz'>Try quiz again?</a>
+                  </p>";
+    return $response;
+}
