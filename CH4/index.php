4714b4493ae50b18325684d6d8f6a65b6bdfe688
PatelShirin <patel.shirin@outlook.com>
2017-04-10 23:37:09 -0400

    minor changes

--- CH4/index.php
+++ CH4/index.php
@@ -2,10 +2,10 @@
 //complete code for index.php
 error_reporting( E_ALL );
 ini_set( "display_errors", 1 );
-include_once "ch4/classes/Page_Data.class.php";
+include_once "classes/Page_Data.class.php";
 $pageData = new Page_Data();
 $pageData->title = "Dynamic image gallery";
-$pageData->content = include_once "ch4/views/navigation.php";
+$pageData->content = include_once "views/navigation.php";
 $userClicked = isset($_GET['page']);
 
 if ( $userClicked ) {
@@ -13,6 +13,6 @@ $fileToLoad = $_GET['page'];
 } else {
 $fileToLoad = "gallery";
 }
-$pageData->content .=include_once "ch4/views/$fileToLoad.php";
-$page = include_once "ch4/templates/page.php";
+$pageData->content .=include_once "views/$fileToLoad.php";
+$page = include_once "templates/page.php";
 echo $page;
