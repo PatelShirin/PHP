193ca6104c8fda6501914630cdf091a3d5b4c565
PatelShirin <patel.shirin@outlook.com>
2017-04-11 12:35:31 -0400

    format text

--- CH4/views/gallery.php
+++ CH4/views/gallery.php
@@ -2,23 +2,42 @@
 //complete source code for views/gallery.php
 //function call
 return showImages();
+
 //function defintion
 function showImages(){
-$out = "<h1>Image Gallery</h1>";
-$out .= "<ul id='images'>";
-$folder = "img";
-$filesInFolder = new DirectoryIterator( $folder);
-while ( $filesInFolder->valid() ) {
-$file = $filesInFolder->current();
-$filename = $file->getFilename();
-$src = "$folder/$filename";
-$fileInfo = new Finfo( FILEINFO_MIME_TYPE );
-$mimeType = $fileInfo->file( $src );
-if ( $mimeType === 'image/jpeg' ) {
-$out .= "<li><img src='$src' /></li>";
-}
-$filesInFolder->next();
-}
-$out .= "</ul>";
-return $out;
+	
+	$out = "<h1>Image Gallery</h1>";
+	
+	$out .= "<ul id='images'>";
+	
+	$folder = "img";
+	
+	$filesInFolder = new DirectoryIterator( $folder);
+	
+	while ( $filesInFolder->valid() ) {
+		
+		$file = $filesInFolder->current();
+		
+		$filename = $file->getFilename();
+		
+		$src = "$folder/$filename";
+		
+		$fileInfo = new Finfo( FILEINFO_MIME_TYPE );
+		
+		$mimeType = $fileInfo->file( $src );
+		
+		if ( $mimeType === 'image/jpeg' ) {
+			
+			$out .= "<li><img src='$src' /></li>";
+			
+		}
+		
+		$filesInFolder->next();
+		
+	}
+	
+	$out .= "</ul>";
+	
+	return $out;
+	
 }
