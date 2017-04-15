bb4dd70981a44509eb1a2119e20c92dfe3da7c18
PatelShirin <patel.shirin@outlook.com>
2017-04-15 00:02:46 -0400

    uploader class and formatted code

--- CH4/views/upload.php
+++ CH4/views/upload.php
@@ -3,17 +3,21 @@
 //$newImageSubmitted is TRUE if form was submitted, otherwise FALSE
 $newImageSubmitted = isset( $_POST['new-image'] );
 if ( $newImageSubmitted ) {
-//this code runs if form was submitted
-$output = upload();
-} else {
-//this runs if form was NOT submitted
-$output = include_once "views/upload-form.php";
+	//t	his code runs if form was submitted
+	$output = upload();
 }
+else {
+	//t	his runs if form was NOT submitted
+	$output = include_once "views/upload-form.php";
+}
+
 return $output;
+
 //declare new function
 function upload(){
-$out = "<pre>";
-$out .=print_r($_FILES, true);
-$out .= "</pre>";
-return $out;
+	$out = "<pre>";
+	$out .=print_r($_FILES, true);
+	$out .= "</pre>";
+	return $out;
+	
 }
