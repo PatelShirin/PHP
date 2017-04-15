fe02f8f7a12922c452c4a0d2e68bcc2050817f9b
PatelShirin <patel.shirin@outlook.com>
2017-04-14 23:43:03 -0400

    upload images

--- CH4/views/upload.php
+++ CH4/views/upload.php
@@ -1,4 +1,19 @@
 <?php
 //complete source code for views/upload.php
-$output=include_once "views/upload-form.php";
+//$newImageSubmitted is TRUE if form was submitted, otherwise FALSE
+$newImageSubmitted = isset( $_POST['new-image'] );
+if ( $newImageSubmitted ) {
+//this code runs if form was submitted
+$output = upload();
+} else {
+//this runs if form was NOT submitted
+$output = include_once "views/upload-form.php";
+}
 return $output;
+//declare new function
+function upload(){
+$out = "<pre>";
+$out .=print_r($_FILES, true);
+$out .= "</pre>";
+return $out;
+}
