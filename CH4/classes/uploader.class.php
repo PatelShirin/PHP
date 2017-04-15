b82ae0c2c4824e7ac707c4ee6dc53fe216c6173d
PatelShirin <patel.shirin@outlook.com>
2017-04-15 00:10:02 -0400

    uploader class and functions

--- CH4/classes/uploader.class.php
+++ CH4/classes/uploader.class.php
@@ -1,15 +1,33 @@
 <?php
+//complete code for classes/Uploader.class.php
 class Uploader {
+	
 	private $filename;
 	private $fileData;
 	private $destination;
+	
+	//d	eclare a constructor method
+	public function __construct( $key ) {
+		$this->filename = $_FILES[$key]['name'];
+		$this->fileData = $_FILES[$key]['tmp_name'];
+	}
 	
 	public function saveIn( $folder ) {
 		$this->destination = $folder;
 	}
 	
 	public function save(){
-		//n		o code here yet
+		$folderIsWriteAble = is_writable( $this->destination );
+		if( $folderIsWriteAble ){
+			$name = "$this->destination/$this->filename";			
+			$succes = move_uploaded_file( $this->fileData, $name );
+		}
+		else {
+			trigger_error("cannot write to $this->destination");
+			$succes = false;
+		}
+		return $succes;
 	}
-	
 }
+
+
--- CH4/views/upload.php
+++ CH4/views/upload.php
@@ -2,6 +2,7 @@
 //complete source code for views/upload.php
 //$newImageSubmitted is TRUE if form was submitted, otherwise FALSE
 $newImageSubmitted = isset( $_POST['new-image'] );
+
 if ( $newImageSubmitted ) {
 	//t	his code runs if form was submitted
 	$output = upload();
@@ -10,14 +11,24 @@ else {
 	//t	his runs if form was NOT submitted
 	$output = include_once "views/upload-form.php";
 }
-
 return $output;
+
 
 //declare new function
 function upload(){
-	$out = "<pre>";
-	$out .=print_r($_FILES, true);
-	$out .= "</pre>";
-	return $out;
+	include_once "classes/Uploader.class.php";
 	
+	//i	mage-data is the name attribute used in <input type='file' />
+	$uploader = new Uploader( "image-data" );
+	$uploader->saveIn("img");	
+	$fileUploaded = $uploader->save();
+	
+	if ( $fileUploaded ) {
+		$out = "new file uploaded";
+	}
+	else {
+		$out = "something went wrong";
+	}
+	return $out;
 }
+
